84ac8cf6159cb3092f6d908f742a158ab32a7757
Dong <Dsheng7777777@gmail.com>
2022-10-14 07:57:10 -0700

    Initial Commit

---
+++ FlowLayoutTest/FlowLayoutTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlowLayoutTestApp.swift
+//  FlowLayoutTest
+//
+//  Created by Dong on 2022/10/14.
+//
+
+import SwiftUI
+
+@main
+struct FlowLayoutTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
